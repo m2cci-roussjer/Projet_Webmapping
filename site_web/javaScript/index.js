7e828358d2ffe6661413c4da9293a44dcc9184e8
m2cci-roussjer <jeremy.rousseau.pro@outlook.fr>
2023-03-19 18:27:54 +0100

    Modification de index.html : ajout de carte et de texte

--- site_web/javaScript/index.js
+++ site_web/javaScript/index.js
@@ -1,43 +1,97 @@
+import 'ol/ol.css';
 import '../style.css';
-import {Map, View} from 'ol';
+import Map from 'ol/Map';
+import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
+import VectorLayer from 'ol/layer/Vector';
 import OSM from 'ol/source/OSM';
-import { fromLonLat } from 'ol/proj';
 import GeoJSON from 'ol/format/GeoJSON';
 import VectorSource from 'ol/source/Vector';
-/* 
-import KML from 'ol/format/KML';
+import { fromLonLat } from 'ol/proj';
 import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style';
-import LayerSwitcher from 'ol-layerswitcher';
-import LayerGroup from 'ol/layer/Group';
-import Legend from 'ol-ext/legend/Legend';
-import CtrLegend from 'ol-ext/control/Legend';
-import Popup from 'ol-popup'; */
+import Popup from 'ol-popup';
 
-const osm = new TileLayer({
-  className: 'bw',
-  title: 'OSM',
+// Créer la couche du fond de carte OpenStreetMap
+const osmLayer = new TileLayer({
+  title: 'OpenStreetMap',
   type: 'base',
   visible: true,
-  source: new OSM(),
-});
-/*Exemple */
-const pump_at_arcgisonline = new VectorSource({
-  url: 'https://dservices9.arcgis.com/nuNzn0OMg75uWEMq/arcgis/services/pumps_/WFSServer?service=wfs&request=getcapabilities',
-  format: new GeoJSON(),
+  source: new OSM()
 });
 
-const pumpLayer = new VectorLayer({
-  source: pump_at_arcgisonline
+// Créer la couche de style commune Isère
+const styleComIsère = function(feature) {
+  const pm12 = feature.get('PMUN12');
+  let color = '';
+  if (pm12 <= 3283) {
+    color = '#fee5d9';
+  } else if (pm12 <= 9944) {
+    color = '#fcae91';
+  } else if (pm12 <= 22050) {
+    color = '#fb6a4a';
+  } else if (pm12 <= 38105) {
+    color = '#de2d26';
+  } else if (pm12 <= 158346) {
+    color = '#a50f15';
+  } else {
+    color = '#000000';
+  }
+  return new Style({
+    fill: new Fill({
+      color: color
+    }),
+    stroke: new Stroke({
+      color: '#333',
+      width: 1
+    })
+  });
+};
+
+// Créer la couche de commune ISère GeoJSON
+const comIsereLayer = new VectorLayer({
+  source: new VectorSource({
+    url: '../data/COMMUNE_Isere.geojson',
+    format: new GeoJSON()
+  }),
+  style: styleComIsère ,
+  title: 'Commune en Isère',
+  visible: true
 });
 
+
+// Créer la carte OpenLayers pour l'Isère
 const map = new Map({
   target: 'map',
-  layers: [osm],
+  layers: [
+    osmLayer,
+    comIsereLayer
+  ],
   view: new View({
-    center: fromLonLat([45.18785481984832, 5.724914730680832]),
-    zoom: 2
-  })
+    center: fromLonLat([5.73, 45.18]),
+    zoom: 9
+  }),
 });
 
-map.addLayer(pumpLayer);
+// Ajout Popup au survol de la souris
+const popup = new Popup();
+map.addOverlay(popup);
+
+map.on('pointermove', function(evt) {
+  map.forEachFeatureAtPixel(evt.pixel, function (feature, layer) {
+    let nom = feature.get('nom');
+    let PMUN12 = feature.get('PMUN12');
+    let PMUN06 = feature.get('PMUN06');
+    let Superficie = feature.get('Surface_km2');
+
+    popup.show(evt.coordinate, '<h7><b>' + nom + '</b></h7> <br><h8>Population en 2012 : </h8>'+PMUN12+'<br><h8>Population en 2006 : </h8>'+PMUN06+'<br><h8>Superficie en km² : </h8>'+Superficie);
+  }, // Dictionnary of options
+    {
+      layerFilter: function (layerCandidate) {
+        return layerCandidate.get('title') == 'Commune en Isère'
+      }
+    });
+});
+
+
+
+
