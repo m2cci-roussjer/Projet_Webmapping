82750a12f2d3ccffc9fda3c049321dd313463826
m2cci-roussjer <jeremy.rousseau.pro@outlook.fr>
2023-03-20 22:04:59 +0100

    Modification d'index.html : ajout du paragraphe sur les données

--- site_web/javaScript/index.js
+++ site_web/javaScript/index.js
@@ -18,6 +18,12 @@ const osmLayer = new TileLayer({
   visible: true,
   source: new OSM()
 });
+const osmLayer2 = new TileLayer({
+  title: 'OpenStreetMap',
+  type: 'base',
+  visible: true,
+  source: new OSM()
+});
 
 // Créer la couche de style commune Isère
 const styleComIsère = function(feature) {
@@ -47,7 +53,7 @@ const styleComIsère = function(feature) {
   });
 };
 
-// Créer la couche de commune ISère GeoJSON
+// Créer la couche de commune Isère GeoJSON
 const comIsereLayer = new VectorLayer({
   source: new VectorSource({
     url: '../data/COMMUNE_Isere.geojson',
@@ -57,7 +63,6 @@ const comIsereLayer = new VectorLayer({
   title: 'Commune en Isère',
   visible: true
 });
-
 
 // Créer la carte OpenLayers pour l'Isère
 const map = new Map({
@@ -92,6 +97,78 @@ map.on('pointermove', function(evt) {
     });
 });
 
+//-------------------------------------------
+// Créer la couche de style commune Rhone
+const styleComRhone = function(feature) {
+  const pm12 = feature.get('PMUN12');
+  let color = '';
+  if (pm12 <= 4123) {
+    color = '#fee5d9';
+  } else if (pm12 <= 13599) {
+    color = '#fcae91';
+  } else if (pm12 <= 31493) {
+    color = '#fb6a4a';
+  } else if (pm12 <= 146282) {
+    color = '#de2d26';
+  } else if (pm12 <= 496343) {
+    color = '#a50f15';
+  } else {
+    color = '#000000';
+  }
+  return new Style({
+    fill: new Fill({
+      color: color
+    }),
+    stroke: new Stroke({
+      color: '#333',
+      width: 1
+    })
+  });
+};
+
+// Créer la couche de commune Rhone GeoJSON
+const comRhoneLayer = new VectorLayer({
+  source: new VectorSource({
+    url: '../data/COMMUNE_Rhone.geojson',
+    format: new GeoJSON()
+  }),
+  style: styleComRhone ,
+  title: 'Commune dans le Rhône',
+  visible: true
+});
+
+// Créer la carte OpenLayers pour le Rhone
+const map2 = new Map({
+  target: 'map2',
+  layers: [
+    osmLayer2,
+    comRhoneLayer
+  ],
+  view: new View({
+    center: fromLonLat([4.82, 45.75]),
+    zoom: 9
+  }),
+});
+
+// Ajout Popup au survol de la souris
+const popup2 = new Popup();
+map2.addOverlay(popup2);
+
+map2.on('pointermove', function(evt) {
+  map2.forEachFeatureAtPixel(evt.pixel, function (feature, layer) {
+    let nom = feature.get('nom');
+    let PMUN12 = feature.get('PMUN12');
+    let PMUN06 = feature.get('PMUN06');
+    let Superficie = feature.get('Surface_km2');
+
+    popup2.show(evt.coordinate, '<h7><b>' + nom + '</b></h7> <br><h8>Population en 2012 : </h8>'+PMUN12+'<br><h8>Population en 2006 : </h8>'+PMUN06+'<br><h8>Superficie en km² : </h8>'+Superficie);
+  }, // Dictionnary of options
+    {
+      layerFilter: function (layerCandidate) {
+        return layerCandidate.get('title') == 'Commune dans le Rhône'
+      }
+    });
+});
 
 
 
